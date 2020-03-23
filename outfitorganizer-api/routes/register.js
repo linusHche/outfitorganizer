190181393883aa8e0e418b11978b1305782cf780
linusHche <linushung28@gmail.com>
2020-03-22 21:42:49 -0700

    Added jwt signing and refactored signin and register functions into individual route files

---
+++ outfitorganizer-api/routes/register.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (pg, bcrypt) => {
+	router.post('/', async (req, res) => {
+		try {
+			const hash = await bcrypt.hash(req.body.password, 10);
+			const data = pg
+				.insert({
+					name: req.body.name,
+					hash
+				})
+				.table('user')
+				.returning('id');
+			res.send(data);
+		} catch (err) {
+			console.error(err);
+		}
+	});
+	return router;
+};
---
+++ outfitorganizer-api/routes/signin.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (pg, bcrypt, jwt) => {
+	const signJWT = name => {
+		return (
+			'Bearer ' +
+			jwt.sign({ name }, process.env.SECRET_KEY, { expiresIn: '1h' })
+		);
+	};
+	router.post('/', async (req, res) => {
+		const loginUser = await pg('user')
+			.where('name', req.body.name)
+			.first();
+		if (loginUser === undefined) {
+			return res.status(404).json('User not found');
+		}
+		const result = await bcrypt.compare(req.body.password, loginUser.hash);
+		if (result === true) {
+			const token = signJWT(req.body.name);
+			return res.json({ token });
+		}
+		return res.status(403).json('Incorrect password');
+	});
+	return router;
+};
